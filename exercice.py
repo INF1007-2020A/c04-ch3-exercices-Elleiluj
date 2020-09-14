c4aaaa64a52635bab4540673b6cfb4fb7f8d45b0
Elleiluj <julie.labbe@polymtl.ca>
2020-09-14 23:26:58 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -21,7 +21,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    return angle_rads/60
+    return 
 
 
 def to_celsius(temperature: float) -> float:
